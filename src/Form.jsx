4382f46a211e7f0d262bb0f15fded0df56169b69
Shivam Sengar <shivamsengar995627@gmail.com>
2023-12-25 16:24:38 +0530

    update

--- src/Form.jsx
+++ src/Form.jsx
@@ -31,56 +31,57 @@ const Form = ({ setShowForm }) => {
         bottom: 0,
       }}
     >
-      <form
-        className="modal-content"
-        onClick={(e) => e.stopPropagation()}
-        onSubmit={handleSubmit}
-        style={{
-          height: "20rem",
-          position: "absolute",
-          left: "50%",
-          bottom: 10,
-          transform: "translate(-50%)",
-          backgroundColor: "#e4e0e0",
-          padding: "1rem 2rem",
-        }}
-      >
-        <label htmlFor="username">Username:</label>
-        <br />
-        <input type="text" id="username" required />
-        <br />
-        <label htmlFor="email">Email Address:</label>
-        <br />
-        <input type="email" id="email" title="hello" required />
-        <br />
-        <label htmlFor="phone">Phone Number:</label>
-        <br />
-        <input
-          type="tel"
-          id="phone"
-          required
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-        />
-        <br />
-        <label htmlFor="dob">Date of Birth:</label>
-        <br />
-        <input
-          type="date"
-          id="dob"
-          required
-          value={dob}
-          onChange={(e) => setDob(e.target.value)}
-        />
-        <br />
-        <button
-          className="submit-button"
-          type="submit"
-          style={{ marginBlock: "2rem" }}
+      <div className="modal-content">
+        <form
+          onClick={(e) => e.stopPropagation()}
+          onSubmit={handleSubmit}
+          style={{
+            height: "20rem",
+            position: "absolute",
+            left: "50%",
+            bottom: 10,
+            transform: "translate(-50%)",
+            backgroundColor: "#e4e0e0",
+            padding: "1rem 2rem",
+          }}
         >
-          Submit
-        </button>
-      </form>
+          <label htmlFor="username">Username:</label>
+          <br />
+          <input type="text" id="username" required />
+          <br />
+          <label htmlFor="email">Email Address:</label>
+          <br />
+          <input type="email" id="email" title="hello" required />
+          <br />
+          <label htmlFor="phone">Phone Number:</label>
+          <br />
+          <input
+            type="tel"
+            id="phone"
+            required
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <br />
+          <label htmlFor="dob">Date of Birth:</label>
+          <br />
+          <input
+            type="date"
+            id="dob"
+            required
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
+          />
+          <br />
+          <button
+            className="submit-button"
+            type="submit"
+            style={{ marginBlock: "2rem" }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
